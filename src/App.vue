c55e0b736da790e051c2d7880cb66b95bd310bb4
cutbm2015 <cutbm2015@gmail.com>
2023-07-26 13:57:02 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,21 @@
+<script setup>
+import { RouterView } from "vue-router";
+</script>
+
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="nav">
+    <router-link to="/">Home</router-link>
+    |
+    <router-link to="/about">About</router-link>
+  </div>
+  <router-view/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  // components: {HelloWorld},
 }
 </script>
 
