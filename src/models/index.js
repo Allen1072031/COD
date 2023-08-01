77f2813d5c51060317260e935f7e5ed9992256af
cutbm2015 <cutbm2015@gmail.com>
2023-08-01 23:03:16 +0800

    fix database connection error (file does not exist)

---
+++ src/models/index.js
@@ -0,0 +1,11 @@
+require('dotenv').config()
+
+const mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+
+const db = {};
+db.mongoose = mongoose;
+db.url = process.env.MONGODB_URL;
+// db.tutorials = require("./tutorial.model.js")(mongoose);
+
+module.exports = db;
