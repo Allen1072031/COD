6f19430a6a8d2364b979d5ee3f2881de5d76271f
cutbm2015 <cutbm2015@gmail.com>
2023-08-03 23:15:36 +0800

    change websocket endpoint

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    client: {
+      webSocketURL: 'ws://64.176.45.205:8080/ws',
+    },
+  }
 })
